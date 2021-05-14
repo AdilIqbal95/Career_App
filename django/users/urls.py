27cd6b69197cd4668131865ae4d25f7957aac0ed
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-14 17:40:29 +0100

    add additional routes

--- django/users/urls.py
+++ django/users/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
-    path('users/', views.index, name='index'), # route for /adoption/
+    path('users/', views.users, name='index'),
+    path('users/<int:id>', views.user, name='show'),
+    path('users/<int:id>/applications', views.user_applications, name='applications'),
     path("login/", auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path("logout/", auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path("register/", views.register, name='register'),
