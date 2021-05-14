2b56a6698e515e8d9b821f0c21c7cbd544f71df0
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-14 17:40:50 +0100

    temporarily disable login

--- django/users/views.py
+++ django/users/views.py
@@ -1,14 +1,14 @@
 from django.core import serializers
 from django.http.response import JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
 from .models import User, Profile, Application
 
-def index(req):
-    return render(req, 'base.html') 
+# def index(req):
+#     return render(req, 'base.html') 
 
 def register(req):
     if req.method == 'POST':
@@ -24,7 +24,7 @@ def register(req):
     
     return render(req, 'register.html', {'form': form})
 
-@login_required
+# @login_required
 def users(req):
     if req.method == 'GET':
         try:
@@ -39,15 +39,30 @@ def users(req):
         except Exception:
             return JsonResponse({'err': 'Could not create new user.'})
 
+def user(req, id):
+    if req.method == 'GET':
+        try:
+            # user = User.objects.get(pk = id)
+            user = get_object_or_404(User, pk = id)
+            return JsonResponse(user)
+        except Exception as err:
+            print(err)
+            return JsonResponse({'err': 'Could not retrieve user.'}, status=404)
+    # elif req.method == 'POST':
+    #     try:
+    #         user = User.objects.create(req.POST)
+    #         return JsonResponse(user)
+    #     except Exception:
+    #         return JsonResponse({'err': 'Could not create new user.'})
 
 
-# @login_required
-# def user_applications(req):
-#     if req.method == 'GET':
-#         try:
-#             user = User.objects.get(username=req.GET["username"])
-#             applications = Application.objects.get(req.GET)
-#             return JsonResponse(list(users), safe=False)
-#         except Exception:
-#             return JsonResponse({'err': 'Could not retrieve users.'})
+@login_required
+def user_applications(req, id):
+    if req.method == 'GET':
+        try:
+            user = User.objects.get(pk=id)
+            applications = Application.objects.filter(user_profile = user.id)
+            return JsonResponse(list(applications), safe=False)
+        except Exception:
+            return JsonResponse({'err': 'Could not retrieve user applications.'})
     
