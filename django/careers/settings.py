1021c8957ca78a905882f007aeb77dd2a6e10bbd
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-14 11:46:45 +0100

    fix path construction

--- django/careers/settings.py
+++ django/careers/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,5 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # LOCAL MEDIA STORAGE
 
-MEDIA_ROOT =  BASE_DIR + 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
