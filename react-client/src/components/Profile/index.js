c3b01313e9844246d225feb6e2db00aa6e0d47cc
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-14 10:34:34 +0100

    :sparkles: create new component for profile

---
+++ react-client/src/components/Profile/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Profile = () => {
+ 
+    return (
+        <>
+            <header>
+                <h1>Profile</h1>
+            </header>
+        </>
+    )
+}
+
+export default Profile;
