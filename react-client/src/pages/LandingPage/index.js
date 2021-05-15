d9c28d357bdf356adba4f60864ceaf4796e73f42
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-15 22:12:47 +0100

    :boom: :lipstick::ambulance: navlink for login/register paths, login/register forms, event handlers and conditional rendering

--- react-client/src/pages/LandingPage/index.js
+++ react-client/src/pages/LandingPage/index.js
@@ -1,20 +1,85 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useHistory, NavLink } from 'react-router-dom';
 
 const LandingPage = () => {
+    const [registrationStatus, setRegistrationStatus] = useState(false);
     const history = useHistory();
 
-    function handleSubmit() {
+    function handleLogin() {
         history.push("/home");
+    }
+
+    function handleRegister() {
+        alert('registered! please login')
+        history.push("/login");
+        location.reload();
+    }
+
+    function nav() {
+
+        return (
+            <nav role="navigation">
+                <NavLink to='/login' activeClassName="current" onClick={loginClicked}>Login</NavLink>
+                <NavLink to='/register' activeClassName="current" onClick={registerClicked}>Register</NavLink>
+            </nav >
+        );
+    }
+
+    function registerClicked() {
+        setRegistrationStatus(true)
+    }
+
+    function loginClicked() {
+        setRegistrationStatus(false)
+    }
+
+    function Login() {
+        return (
+            <form>
+                <label>
+                    <input type="text" placeholder="username" />
+                </label>
+                <label>
+                    <input type="password" placeholder="password" />
+                </label>
+                <div className="button-container">
+                    <button onClick={handleLogin}>Login</button>
+                </div>
+            </form>
+        )
+    }
+
+    function Register() {
+        return (
+            <form>
+                <label>
+                    <input type="text" placeholder="full name" />
+                </label>
+                <label>
+                    <input type="text" placeholder="username" />
+                </label>
+                <label>
+                    <input type="password" placeholder="password" />
+                </label>
+                <label>
+                    <input type="password" placeholder="confirm password" />
+                </label>
+                <div className="button-container">
+                    <button onClick={handleRegister}>Register</button>
+                </div>
+            </form>
+        )
     }
 
     return (
         <>
-            <header>
-                <h1>Login or Register</h1>
-                <button onClick={handleSubmit}>Login</button>
-                
-            </header>
+            <main id="login">
+                {nav()}
+                {registrationStatus ? <Register /> : <Login />}
+
+                <div className="background"></div>
+
+            </main>
         </>
     )
 }
