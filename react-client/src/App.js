5b2647dad11468a6c5de1ed43875d8a17b822075
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-15 21:59:17 +0100

    :truck: add /login and /register paths

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import * as Pages from './pages';
 import { Footer } from './layout'
-
-// import './style.css';
 
 function App() {
 
   return (
     <>
-      {/* <Pages.Home /> */}
       <Switch>
       <Route path="/home">
           <Pages.Home />
@@ -17,7 +14,12 @@ function App() {
       <Route exact path="/">
           <Pages.LandingPage />
         </Route>
-     
+        <Route path="/login">
+          <Pages.LandingPage />
+        </Route>
+        <Route exact path="/register">
+          <Pages.LandingPage />
+        </Route>
         <Route>
           <Pages.NotFound />
         </Route>
