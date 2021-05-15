03776a46780bfb0d06cf5b1ab10f1333682913df
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-15 14:32:56 +0100

    :alembic: routing works but isnt a good solution

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,30 +1,33 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import * as Pages from './pages';
 import { Footer } from './layout'
 
 // import './style.css';
 
-function App(){
+function App() {
 
-    return(
-      <>
-         <Pages.Home />
-           {/* <Switch>
-          <Route exact path="/">
-            <Pages.Home />
-          </Route>
-          <Route path="/about">
-            <Pages.About />
-          </Route>
-          <Route>
-            <Pages.NotFound />
-          </Route>
-        </Switch> */}
-        <Footer />
-      </>
-    );
-  
-  };
-  
+  return (
+    <>
+      {/* <Pages.Home /> */}
+      <Switch>
+      <Route exact path="/login">
+          <Pages.LandingPage />
+        </Route>
+      <Route path="/">
+          <Pages.Home />
+        </Route>
+        {/* <Route exact path="/">
+            <Redirect to="/login" />
+            </Route> */}
+        <Route>
+          <Pages.NotFound />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
+  );
+
+};
+
 export default App;
