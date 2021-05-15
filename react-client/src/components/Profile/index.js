1f716c432213a9b3c59be5e48fef7985847516f9
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-15 17:48:09 +0100

    :lipstick: begin basic styling for profile sidebar

--- react-client/src/components/Profile/index.js
+++ react-client/src/components/Profile/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
 const Profile = () => {
- 
+
     return (
         <>
-        <div className="profile-container">
-            <header>
-                <h1>Profile</h1>
-            </header>
-
-            <main id="profile">
-
-                <p>hi</p>
-            </main>
-        </div>
+            <div className="profile-container">
+                <main id="profile">
+                    <img src="http://comic-cons.xyz/wp-content/uploads/Star-Wars-avatar-icon-Jabba-the-Hutt.png" class="brand-logo"></img>
+                    <div class="username">Jobba</div>
+                    <div class="inputs">
+                        <label>Bio</label>
+                        <input type="bio" placeholder="give a short description" />
+                        <button type="submit">Save</button>
+                    </div>
+                </main>
+            </div>
         </>
     )
 }
