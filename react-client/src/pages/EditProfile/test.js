b54525f69cc2fcff03ea855cbd4d4b756c717503
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-16 15:49:46 +0100

    :white_check_mark: add basic tests for headings and routing

---
+++ react-client/src/pages/EditProfile/test.js
@@ -0,0 +1,9 @@
+import { screen } from '@testing-library/react';
+import EditProfile from '.';
+
+describe('EditProfile', () => {
+    test('it shows page header', () => {
+        render(<EditProfile />)
+        expect(screen.getByRole('heading').textContent).toContain('Edit Your Profile');
+    })
+})
