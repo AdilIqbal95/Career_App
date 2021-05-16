b84dd63cda9e73a3c6f49ed39d1648b560a7231d
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-16 16:18:38 +0100

    create admin user and register

--- django/users/admin.py
+++ django/users/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Profile, Application
+from .models import User, Profile, Application
 
+class UserAdmin(admin.ModelAdmin):
+    model = User
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Profile)
 admin.site.register(Application)
 
