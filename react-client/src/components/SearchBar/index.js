2caeca67aa34f6e1a24bb1c9fed80e11c7cc92ae
“AdilIqbal95” <adil.iqbal@hotmail.co.uk>
2021-05-16 22:19:22 +0100

    created job card component files

--- react-client/src/components/SearchBar/index.js
+++ react-client/src/components/SearchBar/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const SearchBar = () => {
- 
+    
     return (
         <>
-        <div className="SearchBar-container">
+        <div id="SearchBar-container">
           <i id="search" class="fa fa-search" aria-hidden="true"></i>
           <input className= "SearchBar" type='text'></input>
         </div>
