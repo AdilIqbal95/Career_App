193556eab9f4007ae5b95f67ee725d9c1e35686b
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-16 16:34:42 +0100

    add custom user serializer with post method

--- django/users/serializers.py
+++ django/users/serializers.py
@@ -1,18 +1,20 @@
-from django.contrib.auth.models import User,Group
 from django.db.models import fields
-from .models import Profile, Application
+from .models import User, Profile, Application
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'groups']
+        fields = ('id', 'email', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
 
-
-class GroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['id', 'name']
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data) 
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
