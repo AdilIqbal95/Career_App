f2ad0871d5d9c5f29112bd0edb5798325f24ced1
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-17 11:05:45 +0100

    add basic fields

--- django/rewards/models.py
+++ django/rewards/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Reward (models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    point_cost = models.PositiveSmallIntegerField(default=1)
+    
+      
+    def __str__(self):
+        return self.title
+
