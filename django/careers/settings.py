bd5ac5a79a8b2edb2f1c71beeacc3265225ef0d5
“AdilIqbal95” <adil.iqbal@hotmail.co.uk>
2021-05-17 09:57:22 +0100

    Setting up files for server deployment

--- django/careers/settings.py
+++ django/careers/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -147,6 +148,10 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # LOCAL MEDIA STORAGE
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
