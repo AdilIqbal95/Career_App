33f513eb7808074fa68d0e9e61000c6ff4df6d2f
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-17 19:23:36 +0100

    :white_check_mark: :recycle: refactor tests since useContext to pass again!

--- react-client/src/pages/LandingPage/test.js
+++ react-client/src/pages/LandingPage/test.js
@@ -1,22 +1,22 @@
 import { screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import LandingPage from '.';
+import 'jest-localstorage-mock';
 
 describe('LandingPage', () => {
+    let wrapper;
+
     beforeEach(() => {
-        render(<LandingPage />, { wrapper: MemoryRouter });
-    });
+        jest.resetAllMocks()
+        wrapper = ({ children }) => <AuthProvider>{children}</AuthProvider>
+    })
+    // beforeEach(() => {
+    //     render(<LandingPage />, { wrapper: MemoryRouter });
+    // });
 
     test('it renders a nav tag', () => {
+        renderWithProviders(<LandingPage />)
         const nav = screen.queryByRole('navigation');
         expect(nav).toBeInTheDocument();
     })
-
-    describe('login', () => {
-        test('it renders', () => {
-            const loginForm = screen.getByRole('form', { name: 'login' })
-            expect(loginForm).toBeInTheDocument()
-        })
-    })
-
 })
