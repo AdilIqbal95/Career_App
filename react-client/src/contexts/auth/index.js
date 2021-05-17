7803a70b22ee35393d4e2af33573a3c27057bb5d
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-17 16:32:33 +0100

    :passport_control: add auth for login, add validation to form, and error handing

---
+++ react-client/src/contexts/auth/index.js
@@ -0,0 +1,73 @@
+import React, { useState, useContext } from 'react';
+import jwt_decode from "jwt-decode";
+import axios from 'axios';
+
+const AuthContext = React.createContext();
+
+export const useAuthContext = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+    const [ currentUser, setCurrentUser ] = useState(getCurrentUser());
+
+    function getCurrentUser() {
+        let user
+        let token = localStorage.getItem("token");
+        if(token){ 
+            user = jwt_decode(token)
+        }
+        return user
+    }
+
+    const register = userData => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const options = {
+                    headers: { 'Content-Type': 'application/json' }
+                }
+                const { data } = await axios.post(`${process.env.API_URL}/users/register`, userData, options)
+                if (data.err){
+                    throw Error(data.err)
+                }
+                await login(userData);
+                resolve('Registration successful')
+            } catch (err) {
+                reject(`Registration Error: ${err}`);
+            }
+        })
+    }
+
+    const login = userData => {
+        console.log(userData)
+        return new Promise(async (resolve, reject) => {
+            try {
+                const options = {
+                    headers: { 'Content-Type': 'application/json' }
+                }
+                const { data } = await axios.post(`${process.env.API_URL}/api/users/login/`, userData, options)
+                console.log(data.access)
+                if (!data.access) { 
+                    throw new Error(`Login not authorised: ${data.detail}`);
+                }
+                localStorage.setItem("token", data.access);
+                const user = jwt_decode(data.access);
+                setCurrentUser(user);
+                resolve('Login successful')
+            } catch (err) {
+                reject(`Login Error: ${err}`);
+            }
+        })
+    }
+
+    const logout = () => {
+        localStorage.clear();
+        setCurrentUser(null);
+    }
+
+    const auth = { register, login, logout, currentUser }
+
+    return (
+        <AuthContext.Provider value={auth}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
