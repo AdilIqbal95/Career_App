719bd9fbaec19d833df7c2e64aef975d29beecba
“AdilIqbal95” <adil.iqbal@hotmail.co.uk>
2021-05-17 22:53:22 +0100

    update Jobcard

--- react-client/src/components/JobCard/index.js
+++ react-client/src/components/JobCard/index.js
@@ -5,11 +5,11 @@ function JobCard({ job}){
  
   return (
       <div role="listitem">
-        {/* {id && <a href={`/albums/${id}`}>more info...</a>}<Like /> */}
-        <h2 style={{marginTop: "1rem"}}>{job.Role}|{job.Company}</h2>
+        <h2>{job.Role}|{job.Company}</h2>
         <p>{job.Description}</p>
       </div>
   );
+  
   }
 
 export default JobCard;
