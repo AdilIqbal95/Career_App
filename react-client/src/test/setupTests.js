bc11df9b1af61d25b4f329e9da21f399b419894c
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-17 19:19:40 +0100

    :wrench: update test setup since implementing Auth context

--- react-client/src/test/setupTests.js
+++ react-client/src/test/setupTests.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { AuthProvider } from '../contexts/auth'
+import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 
+const UnAuthedProviders = ({ children }) => {
+    return (
+      <MemoryRouter>
+          <AuthProvider>
+                  {children}
+          </AuthProvider>
+      </MemoryRouter>
+    )
+  }
+
+const renderWithProviders = (ui, options) => render(ui, { wrapper: UnAuthedProviders, ...options })
+
+
 global.React = React;
 global.render = render;
 global.userEvent = userEvent;
+global.renderWithProviders = renderWithProviders
