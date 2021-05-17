a33f0e57a00589922068944b9a89f94969a80c38
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-17 12:26:30 +0100

    allow point additions and subtractions

--- django/rewards/models.py
+++ django/rewards/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+# from users.models import Profile
 # Create your models here.
 class Reward (models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
-    point_cost = models.PositiveSmallIntegerField(default=1)
+    point_change = models.IntegerField(default=0)
     
       
     def __str__(self):
