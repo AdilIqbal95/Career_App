475c8df6f246281d64afd21f07ffcfad38013875
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-18 22:45:51 +0100

    :beers: add a cute gif might delete l8er

--- react-client/src/pages/About/index.js
+++ react-client/src/pages/About/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const About = () => {
- 
+
     return (
         <>
             <header>
                 <h1>About</h1>
+                <p>JobbaHunt is here to help you get your first job!</p>
+                <img style={{ position: "fixed", bottom: "0", marginBottom: "2rem", borderRadius: "15px" }} src="https://igrad-smedia-igrad.netdna-ssl.com/IMAGE/Article-Images/start-here-guides/Start%20Here%20Preparing%20for%20Job%20Hunt/PrepareForJobHunting_Pic1_850x400.gif" />
             </header>
         </>
     )
