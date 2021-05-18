9ed493a32ebc18690b7d8a578ba82f21a5e4f056
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-18 10:41:35 +0100

    add property to allow unique rewards

--- django/rewards/models.py
+++ django/rewards/models.py
@@ -5,6 +5,7 @@ class Reward (models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
     point_change = models.IntegerField(default=0)
+    one_time = models.BooleanField(default=False)
     
       
     def __str__(self):
