050b973e7466cac9441ac84972f1a39e51990518
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-18 17:06:32 +0100

    :sparkles: useEffect function to fetch profile pic

--- react-client/src/components/ProfileImage/index.js
+++ react-client/src/components/ProfileImage/index.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuthContext } from '../../contexts/auth'
 
 const ProfileImage = () => {
     const [editProfPic, setEditProfPic] = useState(true);
     const [formData, setFormData] = useState({})
+    const [userData, setUserData] = useState()
     const [error, setError] = useState("");
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false)
 
     function handleEditProfile(e) {
         e.preventDefault()
@@ -27,7 +28,6 @@ const ProfileImage = () => {
             }
             localStorage.setItem("token", data.access);
             console.log("success! your access token has been updated")
-            picUpdateRetry()
         } catch (err) {
             setLoading(false)
             setError(`❌ Sorry, try again!`)
@@ -77,12 +77,31 @@ const ProfileImage = () => {
         setFormData(e.target.files[0])
     }
 
+    useEffect(() => {
+        async function profileData() {
+            try {
+                let token = localStorage.getItem("token")
+                let userID = localStorage.getItem("user_id")
+                const options = {
+                    headers: { "Authorization": `Bearer ${token}` }
+                };
+                const { data } = await axios.get(`${process.env.API_URL}/api/users/${userID}/profile/`, options)
+                console.log(data)
+                setUserData(data)
+            } catch {
+                console.warn("There's an error!!! Cannot fetch user profile details")
+                refresh()
+            }
+        } profileData()
+    }, []);
+
     return (
         <>
             <div className="pic-wrapper">
                 {error && <div style={{ position: "absolute" }} id="error">{error}</div>}
                 {loading && <div style={{ position: "absolute" }} id="loading">Loading . . .</div>}
-                <img src="http://comic-cons.xyz/wp-content/uploads/Star-Wars-avatar-icon-Jabba-the-Hutt.png" className="profile-pic"></img>
+                {userData ? <img style={{ maxWidth: "130px" }} src={userData.description}></img>
+                    : <img src="http://comic-cons.xyz/wp-content/uploads/Star-Wars-avatar-icon-Jabba-the-Hutt.png" className="profile-pic"></img>}
                 <form aria-label="edit profile pic" className="change-profile-pic" onSubmit={handlePicUpdate}>
                     {editProfPic ?
                         <button onClick={handleEditProfile}> 🖋 </button> :
