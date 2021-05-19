0836fc3fed1613620048074d6eb9133b72bea6fc
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-19 20:53:34 +0100

    :boom: update progress bar with nice colors which updates depending on user application state

--- react-client/src/components/ApplicationCard/index.js
+++ react-client/src/components/ApplicationCard/index.js
@@ -12,20 +12,16 @@ const ApplicationCard = ({ job }) => {
             <article className="myjob-container">
                 <h3><a href={job.url} target="_blank">{job.job_title}</a></h3> <h4>{job.company}</h4>
                 <p>{job.description}</p>
-
                 <ProgressBar>
                 {job.applied && 
                     <ProgressBar striped variant="success" now={25} key={1} label="applied"/>}
                 {job.interviewed &&   
-                    <ProgressBar variant="warning" now={50} key={2} label="interviewed" />}
+                    <ProgressBar now={25} key={2} label="interviewed" />}
                 {job.offered && 
-                    <ProgressBar striped variant="danger" now={75} key={3} label="offered" />}
+                    <ProgressBar striped variant="warning" now={25} key={3} label="offered" />}
                 {job.acccepted && 
-                    <ProgressBar striped variant="danger" now={100} key={3} label="accepted" />}
+                    <ProgressBar striped variant="success" now={25} key={3} label="accepted" />}
                 </ProgressBar>
-
-
-                <ProgressBar now={20} label={`${status}`} />
             </article>
         </>
     )
