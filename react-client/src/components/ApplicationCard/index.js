720f3720435eeff0a3f70abd5b8d7e8d5d12e635
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-19 16:08:55 +0100

    :clown_face: mock functions and progress bar for fetched data from api

--- react-client/src/components/ApplicationCard/index.js
+++ react-client/src/components/ApplicationCard/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ApplicationCard = ({job}) => {
     console.log(job)
-    
+
+    const [score, setScore] = useState(0)
+
+    function progress() {
+        if (job.acccepted === true) {
+            return setScore(100);
+        } else if (job.offered === true) {
+            return setScore(80);
+        } else if (job.interviewed === true) {
+            return setScore(60);
+        } else if (job.applied === true) {
+            return setScore(40);
+        } else if (job.saved === true) {
+            return setScore(20);
+        } else {
+            return setScore(0);
+        }
+    }
+
     return (
         <>
-       
+       <h2><a href={job.url} target="_blank">{job.job_tile}</a></h2> | <h2>{job.company}</h2>
+       <p>{job.description}</p>
+
+       <ProgressBar now={score} label={`${score}%`} />
+
         </>
     )
 }
