74c735229af60298fa0b67f17bda51d8c058d89b
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-19 15:37:21 +0100

    :sparkles: create new component for application card to render a list of user's applied / saved jobs on window load

---
+++ react-client/src/components/ApplicationCard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ApplicationCard = ({job}) => {
+    console.log(job)
+    
+    return (
+        <>
+       
+        </>
+    )
+}
+
+export default ApplicationCard;
