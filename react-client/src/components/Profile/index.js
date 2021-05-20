079374ff5a112f93006df71ecb9430a5f3b1561f
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-20 22:56:25 +0100

    :ambulance: add conditional to fetch
     data

--- react-client/src/components/Profile/index.js
+++ react-client/src/components/Profile/index.js
@@ -51,7 +51,7 @@ const Profile = () => {
                                 <div className="game-stats" style={{ textAlign: "center" }}>
                                     <h5>Daily Streak:</h5>
                                     <div className="calender">
-                                            <BiCalendarAlt /> <p className="number">{userData.daily_streak} </p></div>
+                                        <BiCalendarAlt /> {userData && <p className="number">{userData.daily_streak} </p>}</div>
                                 </div>
 
 
