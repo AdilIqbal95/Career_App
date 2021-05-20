60a44df76ba23da913632213836df9bb810eae8a
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-05-20 23:27:22 +0100

    refactor status update to allow controlled form

--- react-client/src/components/ApplicationCard/index.js
+++ react-client/src/components/ApplicationCard/index.js
@@ -7,24 +7,31 @@ const ApplicationCard = ({ job }) => {
     const { refresh } = useAuthContext();
     const [score, setScore] = useState(0)
     const [error, setError] = useState("")
-    const [ status, setStatus ] = useState(job)
+    const [status, setStatus] = useState(job)
+    const [progress, setProgress] = useState('0');
 
-    const handleUpdateApplication = async (e) => {
-        e.preventDefault();
-        await refresh()
-        try {
-            let token = localStorage.getItem("token")
-            let userID = localStorage.getItem("user_id")
-            const options = {
-                headers: { "Authorization": `Bearer ${token}` }
-            };
-            const sentData = { [e.target.value]: true };
-            const {data} = await axios.patch(`${process.env.API_URL}/api/users/${userID}/applications/${job.id}`, sentData, options )
-            setStatus(data)
-        } catch (err) {
-            setError(`❌ Sorry, try again! ${err}`)
-        }
+    const handleUpdateApplication = (e) => {
+        setProgress( e.target.value )
     }
+
+    useEffect(() => {
+        const updateStatus = async () => {
+            try {
+                let token = localStorage.getItem("token")
+                let userID = localStorage.getItem("user_id")
+                const options = {
+                    headers: { "Authorization": `Bearer ${token}` }
+                };
+                const sentData = { [progress]: true };
+                const { data } = await axios.patch(`${process.env.API_URL}/api/users/${userID}/applications/${job.id}/`, sentData, options)
+                setStatus(data)
+            } catch (err) {
+                setError(`❌ Sorry, try again! ${err}`)
+                await refresh()
+            }
+        }
+        updateStatus()
+    }, [progress]);
 
     return (
         <>
@@ -34,8 +41,8 @@ const ApplicationCard = ({ job }) => {
                 <p>{job.description}</p>
                 <form style={{ display: "flex", justifyContent: "flex-end", textAlign: "center" }}>
                     <label className="application-status" htmlFor="application-status">
-                        <select onChange={handleUpdateApplication} type="application-status" name="application-status">
-                            <option value="0" selected disabled hidden>⚡️ Application Status</option>
+                        <select onChange={handleUpdateApplication} value={progress} type="application-status" name="application-status">
+                            <option value="0" disabled hidden>⚡️ Application Status</option>
                             <option value="1">None</option>
                             <option value="applied">Applied</option>
                             <option value="interviewed">Interviewed</option>
@@ -53,7 +60,7 @@ const ApplicationCard = ({ job }) => {
                         <ProgressBar striped variant="warning" now={25} key={3} label="offered" />}
                     {status.accepted &&
                         <ProgressBar striped variant="success" now={25} key={4} label="accepted" />}
-                </ProgressBar>
+                </ProgressBar>              
                 {error && <div id="error">{error}</div>}
 
             </article>
