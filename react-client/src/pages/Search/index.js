da6f2c13f883ddc463922484054167878f018d47
“AdilIqbal95” <adil.iqbal@hotmail.co.uk>
2021-05-20 12:42:30 +0100

    added getJobs functiona and handleinput function - which fetch data from reed api based on input in searchbar

--- react-client/src/pages/Search/index.js
+++ react-client/src/pages/Search/index.js
@@ -1,22 +1,44 @@
 import React, {useState} from 'react';
 import { Results } from '../../components';
 import data from '../../temp_data'
-
+import { useAuthContext } from "../../contexts/auth";
+import axios from 'axios';
 
 const Search = () => {
     // const [jobList, setJobList] = useState(data)
     const tempData = data
-    const [input, setInput] = useState("")
-
-    const getJobs = () => {
-      setInput()
-
+    const [inputData, setInputData] = useState({input:""})
+    const { refresh } = useAuthContext();
+    const [jobsData, setjobsData] = useState()
+    const handleInput = e => setInputData(prev => ({ ...prev, [e.target.name]: e.target.value }))
+    let input = inputData.input
+    console.log("Input data is: ",inputData)
+    console.log("Input is: ",input)
+    
+    const getJobs = async () => {
+      // setInput()
+      await refresh()
+      try {
+        let token = localStorage.getItem("token")
+        let userID = localStorage.getItem("user_id")
+        const options = {
+          headers: { "Authorization": `Bearer ${token}` }
+        };
+        const response = await axios.get(`${process.env.API_URL}/api/jobs?keywords=${input}`, options)
+        const data = response.data.results
+        const totalResults = response.data.totalResults
+        console.log("job data: ",data)
+        console.log("total results: ",totalResults)
+      } catch (err) {
+        console.error(err.message)
+      }
     }
+    getJobs()
 
     return (
         <>
             <div className="row"id="SearchBar-container">
-              <input className= "SearchBar col" type='text'></input>
+              <input name="input" onChange={handleInput} className= "SearchBar col" type='text'></input>
               <button className="col"><i id="search" class="fa fa-search" aria-hidden="true"></i>
               </button>
             </div>
