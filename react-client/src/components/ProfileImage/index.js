34166a5dba6b47c20cdf022670f758d90f886c55
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-20 11:12:37 +0100

    :sparkles: add loaders/ spinners to all loading states

--- react-client/src/components/ProfileImage/index.js
+++ react-client/src/components/ProfileImage/index.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { useAuthContext } from '../../contexts/auth'
+import Spinner from 'react-bootstrap/Spinner'
 
 const ProfileImage = () => {
     const { refresh } = useAuthContext();
- 
+
     const [editProfPic, setEditProfPic] = useState(true);
     const [formData, setFormData] = useState({})
     const [userData, setUserData] = useState()
@@ -33,11 +34,11 @@ const ProfileImage = () => {
                 }
             }
             await axios.patch(`${process.env.API_URL}/api/users/${userID}/profile/`, data, options)
-            
+
             // update picture reward
             try {
-                await axios.post(`${process.env.API_URL}/api/users/${userID}/rewards/`, {'reward': 3}, options)    
-            } catch (error) {}
+                await axios.post(`${process.env.API_URL}/api/users/${userID}/rewards/`, { 'reward': 3 }, options)
+            } catch (error) { }
             setLoading(false)
         } catch (err) {
             setLoading(false)
@@ -72,9 +73,9 @@ const ProfileImage = () => {
         <>
             <div className="pic-wrapper">
                 {error && <div style={{ position: "absolute" }} id="error">{error}</div>}
-                {loading && <div style={{ position: "absolute" }} id="loading">Loading . . .</div>}
+                {loading && <div style={{ position: "absolute" }} className="loader"><Spinner animation="border" variant="warning" role="status" /></div>}
                 {userData ? <img className="profile-pic" src={userData.profile_image}></img>
-                    : <img src="https://i.imgur.com/tfnVE8n.png" className="profile-pic"></img>}
+                    : <div className="loader"><Spinner animation="border" variant="warning" role="status" /></div>}
                 <form aria-label="edit profile pic" role="edit-profile" className="change-profile-pic" onSubmit={handlePicUpdate}>
                     {editProfPic ?
                         <button onClick={handleEditProfile}> 🖋 </button> :
@@ -82,7 +83,7 @@ const ProfileImage = () => {
                             <button type="submit" > ✔️ </button>
                             <div><input type="file" id="my-profile-pic" onChange={handleInput} name="profile-pic" /></div>
                             {/* <div style={{ position: "relative" }}><button>x</button></div> */}
-                            </>
+                        </>
                     }
                 </form>
             </div>
