c9c3b2fc9b409b2e57d9a61652805b4c37b462dd
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-20 15:07:22 +0100

    :art: fully functioning FinalReward button that renders a gif when clicked

--- react-client/src/components/FinalReward/index.js
+++ react-client/src/components/FinalReward/index.js
@@ -1,41 +1,24 @@
-import axios from "axios";
 import React, { useState } from "react";
 
 function FinalReward({ reward }) {
-    console.log(reward)
-  const [collected, setCollected] = useState(false)
-  const [available, setAvailable] = useState(true);
-  const [error, setError] = useState("")
-  const [ jobbas, setJobbas] = useState(localStorage.getItem('jobbas'));
+  const [gif, setGif] = useState()
 
-  async function collectReward() {
-    let token = localStorage.getItem("token")
-    let userID = localStorage.getItem("user_id")
-    const options = {
-      headers: { "Authorization": `Bearer ${token}` }
-    };
-    try {
-      await axios.post(`${process.env.API_URL}/api/users/${userID}/rewards/`, { "reward": reward.id, "points_change": jobbas }, options)
-      setCollected(true)
-    } catch (err) {
-      setAvailable(false)
-      setError(`❌ Sorry, not enough points!`)
-    }
+  function collectReward() {
+    setGif({ backgroundImage: "url('https://media.giphy.com/media/26tOZ42Mg6pbTUPHW/giphy.gif')", color: "white" })
   }
 
   return (
     <>
-      {/* <section className="reward many-time-reward">
-      <title className="card-title">
-        <h4>{reward.title}</h4>
-          <p style={{color: "#FF3333"}}> -{jobbas}</p>
-            </title>
+      <section className="reward many-time-reward" style={gif}>
+        <title className="card-title">
+          <h4>{reward.title}</h4>
+        </title>
         <p>{reward.description}</p>
-      </section> */}
+        <div className="button-container"><button onClick={collectReward}>Collect!</button></div>
+      </section>
     </>
 
   );
-
 }
 
 export default FinalReward;
