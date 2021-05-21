351ff0a30c8f9e594b5a80546bbc18d731a67eaa
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-21 10:23:04 +0100

    :zap: add DELETE request for job function

--- react-client/src/components/JobCard/index.js
+++ react-client/src/components/JobCard/index.js
@@ -3,54 +3,46 @@ import { useAuthContext } from '../../contexts/auth'
 import axios from 'axios';
 
 function JobCard({ job }) {
-        const { refresh } = useAuthContext();
-        const [formData, setFormData] = useState({
-                job_title: job.jobTitle,
-                company: job.employerName,
-                description: job.jobDescription,
-                url: job.jobUrl,
-                saved: true,
-                applied: false,
-                interviewed: false,
-                offered: false,
-                accepted: false
-            })
-        const [error, setError] = useState("");
-        const handleSaveJob = async () => {
-                await refresh()
-                let token = localStorage.getItem("token")
-                let userID = localStorage.getItem("user_id")
-                try {
-                  
-                    const options = {
-                        headers: { "Authorization": `Bearer ${token}` }
-                    };
-                    console.log(formData)
-                    await axios.post(`${process.env.API_URL}/api/users/${userID}/applications/`, formData, options)
-                    alert('Job Saved!')
-            
-                    // add saved job reward
-                    // try {
-                    //     await axios.post(`${process.env.API_URL}/api/users/${userID}/rewards/`, {'reward': 1}, options)    
-                    // } catch (error) { }
-                } catch (err) {
-                    setError(`❌ Sorry, try again! ${err}`)
+    const { refresh } = useAuthContext();
+    const [saved, setSaved] = useState(false)
+    const [formData, setFormData] = useState({
+        job_title: job.jobTitle,
+        company: job.employerName,
+        description: job.jobDescription,
+        url: job.jobUrl,
+        saved: true,
+        applied: false,
+        interviewed: false,
+        offered: false,
+        accepted: false
+    })
+    const [error, setError] = useState("");
+    const handleSaveJob = async () => {
+        await refresh()
+        let token = localStorage.getItem("token")
+        let userID = localStorage.getItem("user_id")
+        try {
+            const options = {
+                headers: { "Authorization": `Bearer ${token}` }
+            };
+            await axios.post(`${process.env.API_URL}/api/users/${userID}/applications/`, formData, options)
+            setSaved(true)
+        } catch (err) {
+            setError(`❌ Sorry, try again! ${err}`)
         }
-}
+    }
 
-        
-
-  return (
-      <div role="listitem" className="search-container">
-        <h2>{job.jobTitle} | {job.employerName}
-        </h2>
-        <p>{job.jobDescription}</p>
-        <button onClick={handleSaveJob}>Save</button>
-        <button><a href={job.jobUrl}>Apply here!</a></button>
-      </div>
-  );
-
-        
+    return (
+        <div role="listitem" className="search-container">
+            <h2>{job.jobTitle} | {job.employerName}
+            </h2>
+            <p>{job.jobDescription}</p>
+            {saved ?
+                <button style={{background: "rgb(250, 177, 43)"}}>Saved!</button> :
+                <button style={{background: "rgb(250, 177, 43)"}} onClick={handleSaveJob}>Save</button>}
+            <button><a href={job.jobUrl}>Apply here!</a></button>
+        </div>
+    );
 }
 
 export default JobCard;
